56a4b227022b717455fc6287b46ea0c09ae74197
Laba Subagia <labasubagia22@gmail.com>
2021-12-18 08:15:13 +0800

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button type="button" @click="increment">count is: {{ count }}</button>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  props: {
+    msg: String,
+  },
+  setup() {
+    const count = ref(0);
+
+    const increment = () => count.value++;
+
+    return {
+      count,
+      increment,
+    };
+  },
+};
+</script>
+
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
