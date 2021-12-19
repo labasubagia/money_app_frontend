71402d0e638d17d81329ec96e2fa84708bbb46c7
Laba Subagia <labasubagia22@gmail.com>
2021-12-19 13:08:37 +0800

    change font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
